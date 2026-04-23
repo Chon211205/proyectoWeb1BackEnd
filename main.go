27f0f9632e181f40a33fbfc4209e3a486af5cbf8
Chon211205 <lee241203@uvg.edu.gt>
2026-04-23 15:52:48 -0600

    adaptando a railway

--- main.go
+++ main.go
@@ -2,41 +2,40 @@ package main
 
 import (
 	"database/sql"
-	"fmt" 
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "modernc.org/sqlite"
 )
 
-//Main
+// Main
 func main() {
-	//Abre la base de datos llamada series.db
 	db, err := sql.Open("sqlite", "file:series.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
-	//Manejo de errores
 	if err := initDB(db); err != nil {
 		log.Fatal(err)
 	}
 
-	//Router
 	mux := http.NewServeMux()
-	//Registro de rutas y de swagger
+
 	registerRoutes(mux, db)
 	registerSwaggerRoutes(mux)
 	registerUploadRoutes(mux)
 
-	//Mandar las imagenes.
 	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
 
-	//peticiones desde frontend y evitar los errores cors
 	handler := corsMiddleware(mux)
 
-	//Levantar el servidor.
-	fmt.Println("API corriendo en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("API corriendo en puerto %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
