bc92de0ba560b09451fcaf0f146dfff475b69140
Chon211205 <lee241203@uvg.edu.gt>
2026-04-23 20:25:15 -0600

    swagger arreglado talvez

--- swagger.go
+++ swagger.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	_ "embed"
 	"net/http"
-	"os"
 )
+
+var openAPISpec []byte
 
 func registerSwaggerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/openapi.json", serveOpenAPI)
@@ -11,14 +13,9 @@ func registerSwaggerRoutes(mux *http.ServeMux) {
 }
 
 func serveOpenAPI(w http.ResponseWriter, r *http.Request) {
-	// Verifica si el archivo existe
-	if _, err := os.Stat("openapi.json"); os.IsNotExist(err) {
-		http.Error(w, "openapi.json not found", http.StatusNotFound)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	http.ServeFile(w, r, "./openapi.json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(openAPISpec)
 }
 
 func serveSwaggerUI(w http.ResponseWriter, r *http.Request) {
@@ -29,18 +26,6 @@ func serveSwaggerUI(w http.ResponseWriter, r *http.Request) {
   <title>Swagger UI</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
-  <style>
-    html, body {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      background: #f5f7f8;
-    }
-    #swagger-ui {
-      max-width: 1200px;
-      margin: 0 auto;
-    }
-  </style>
 </head>
 <body>
   <div id="swagger-ui"></div>
@@ -49,7 +34,7 @@ func serveSwaggerUI(w http.ResponseWriter, r *http.Request) {
   <script>
     window.onload = () => {
       window.ui = SwaggerUIBundle({
-        url: window.location.origin + "/openapi.json",
+        url: "/openapi.json",
         dom_id: "#swagger-ui"
       });
     };
@@ -59,5 +44,5 @@ func serveSwaggerUI(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(html))
+	_, _ = w.Write([]byte(html))
 }
