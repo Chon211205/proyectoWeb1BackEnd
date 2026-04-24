86bff00e599fd341fe70fe1b6c4b9d874bef8f30
Chon211205 <lee241203@uvg.edu.gt>
2026-04-23 20:48:52 -0600

    swagger

--- swagger.go
+++ swagger.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"net/http"
 )
 
@@ -16,40 +17,33 @@ func registerSwaggerRoutes(mux *http.ServeMux) {
 func serveOpenAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(openAPISpec)
+	_, _ = w.Write(openAPISpec)
 }
 
 func serveSwaggerUI(w http.ResponseWriter, r *http.Request) {
-	html := `<!doctype html>
+	html := fmt.Sprintf(`<!doctype html>
 <html>
 <head>
   <meta charset="utf-8">
   <title>Swagger UI</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.17.14/swagger-ui.css">
 </head>
 <body>
   <div id="swagger-ui"></div>
 
-  <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui-bundle.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
-
+  <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.17.14/swagger-ui-bundle.js"></script>
   <script>
     window.onload = function () {
       SwaggerUIBundle({
-        url: "https://proyectoweb1backend-production.up.railway.app/openapi.json",
-        dom_id: "#swagger-ui",
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        layout: "BaseLayout"
+        spec: %s,
+        dom_id: "#swagger-ui"
       });
     };
   </script>
 </body>
-</html>`
+</html>`, string(openAPISpec))
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(html))
+	_, _ = w.Write([]byte(html))
 }
